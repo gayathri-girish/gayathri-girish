d133a4960e874320204f390de759cc35b874be8a
Gayathri Girish <gayathri120906@gmail.com>
2025-08-15 19:12:39 +0530

    Add files via upload

---
+++ Take input and Print Program.cpp
@@ -0,0 +1,17 @@
+// Program to take a name as input and print "Namah Shivaya , [name] !"
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;          // its a variableto store the name which is taken as input from the writer or user .
+
+    cout << "Enter your name = ";          // it tells the user to enter their name .
+    cin >> name;          // it takes the input from the user and stores it in character variable name .
+
+    cout << "Namah Shivaya , " << name <, "!" << endl;          // it prints the message "Namah Shivaya , [name] !" where [name] is replaced by the name enteres by the user .
+
+    return 0;
+}
