7449a776eba54d284d3a88248d2444232c785e23
Gayathri Girish <gayathri120906@gmail.com>
2025-08-15 19:10:57 +0530

    Add files via upload

---
+++ Print Program .cpp
@@ -0,0 +1,12 @@
+// Program to print "Namah Shivaya !"
+
+
+#include <iostream>                              // includes the iostream library for input and output opearations .
+using namespace std;                            // its used to avoid writing the std::before and after the standard library names .
+
+int main()                                      // its the main function where the program execution begins .
+{
+    cout << "Namah Shivaya !" << endl;          // endl "end the line" is used to move the cursor to the next line after printing that message .
+
+    return 0;                                   // it indicates that the program has successfully executed .
+}
